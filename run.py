711bb3635187a4930306bf53c17d9a5d39b1c38c
MeiryDiniz <>
2023-10-16 18:17:35 +0000

    Add function to have date and revenue data inputed

--- run.py
+++ run.py
@@ -1,26 +1,45 @@
+import pandas as pd
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 import colorama
-from colorama import Fore, Back, Style 
+from colorama import Fore, Back, Style
+from datetime import datetime
 colorama.init(autoreset=True)
-import pandas as pd 
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('farmer-market')
 
-print(Fore.BLUE+Back.WHITE+'Hello!')
 
-worksheet_to_print = SHEET.worksheet('profit')
-data = worksheet_to_print.get_all_values()
-df = pd.DataFrame(data[1:], columns=data[0])
+def get_revenue_input():
+    """
+    Function to have date and revenue value input from the user
+    """
+    print(Fore.GREEN+'Please, use the following examples to enter revenue date:')
+    print(Fore.GREEN+'Please enter the "date" in the format "YYYY-MM-DD"')
+    print(Fore.GREEN+'Please enter the "value" of daily revenue in the format "1.05"')
 
-print(df)
+    date_inf = input('Enter the date here:')
+    date_added = datetime.strptime(date_inf, '%Y-%m-%d').date()
+    worksheet_to_input = SHEET.worksheet('revenue')
+
+    revenue_data = input('Enter the revenue value here:')
+    revenue_value = float(revenue_data)
+
+    row_to_append = [str(date_added), revenue_value]
+    worksheet_to_input.append_row(row_to_append)
+
+    print(f'Date "{date_added}" and "{revenue_value}" added successfully')
+
+    return worksheet_to_input    
+
+
+get_revenue_input()    
