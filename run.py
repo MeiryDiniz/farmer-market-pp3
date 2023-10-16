4ad993c5662ffed34ac3648fc6f839e51cdf7c06
MeiryDiniz <>
2023-10-16 16:32:57 +0000

    Had pandas and colorama libraries installed an tested

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import colorama
+from colorama import Fore, Back, Style 
+colorama.init(autoreset=True)
+import pandas as pd 
 from pprint import pprint
 
 SCOPE = [
@@ -13,8 +17,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('farmer-market')
 
-sales = SHEET.worksheet('revenue')
+print(Fore.BLUE+Back.WHITE+'Hello!')
 
-data = sales.get_all_values()
+worksheet_to_print = SHEET.worksheet('profit')
+data = worksheet_to_print.get_all_values()
+df = pd.DataFrame(data[1:], columns=data[0])
 
-print(data)
+print(df)
