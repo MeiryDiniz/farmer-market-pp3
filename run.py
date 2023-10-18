f8384b017aa52a7c5f234707bca8b5f41df37c3c
MeiryDiniz <>
2023-10-18 17:49:25 +0000

    Add function to sum the values inputted on worksheets

--- run.py
+++ run.py
@@ -20,10 +20,10 @@ SHEET = GSPREAD_CLIENT.open('farmer-market')
 
 def get_worksheet_data():
     """
-    Function to have revenue and month data inputted from the user,
-    and run a while loop to have a correct data inputted by the
-    user. With that the program will keep running just when the correct 
-    data is inputted.
+    Function to have revenue and expense data inputted from the user.
+    A while loop runs to keep program running while correct data is not 
+    inputted by the user. Data inputted is passed to the 
+    validate_month_input(data) function to be validated.
     """
 
 # Add data to 'revenue' worksheet
@@ -44,8 +44,8 @@ format "1.05" or "4" for whole numbers.\n')
         revenue_input = (input(Fore.GREEN+'Enter the revenue value here:\n'))
         if validate_value_input(revenue_input):
             break
-    print(Fore.MAGENTA+f'Data inputted is valid: Month: "{month_input_revenue}"\
-and Revenue: "{revenue_input}"!')
+    print(Fore.MAGENTA+f'Data inputted is valid: Month: \
+"{month_input_revenue}" and Revenue: "{revenue_input}"!')
 
 # Add data to 'expenses' worksheet
     while True:
@@ -65,15 +65,16 @@ format "1.05" or "4" for whole numbers.\n')
         expense_input = (input(Fore.GREEN+'Enter the expense value here:\n'))
         if validate_value_input(expense_input):
             break
-    print(Fore.MAGENTA+f'Data inputted is valid: Month: "{month_input_expense}"\
-and Expense: "{expense_input}"!')
+    print(Fore.MAGENTA+f'Data inputted is valid: Month: \
+"{month_input_expense}" and Expense: "{expense_input}"!')
     return month_input_revenue, revenue_input, month_input_expense, expense_input
 
 
 def validate_month_input(data):
     """
-    Check the data inputted for month_input in the get_worksheet_data
-    function and raises a ValueError if data does not match with data asked
+    Check data inputted for month_input_revenue and  month_input_expense
+    in the get_worksheet_data function, and raises a ValueError if data 
+    does not match with data asked.
     """
     try:
         worksheet_data = SHEET.worksheet('revenue').get_all_values()
@@ -89,14 +90,13 @@ please provide a valid month')
 
 def validate_value_input(data):
     """
-    Check the data inputted for revenue_input in the 
-    get_worksheet_data function, convert data 
-    to a float number and raises a ValueError if data does 
-    not match with data asked
+    Check data inputted for revenue_input and expense_input in the
+    get_worksheet_data function, convert data to a float number and 
+    raises a ValueError if data does not match with data asked.
     """
     try:
         revenue_data = float(data)
-        if revenue_data <= 0:
+        if revenue_data < 0:
             raise ValueError
     except ValueError as e:
         print(Fore.RED+f'Invalid data: {e}Please enter the "value" of daily \
@@ -122,6 +122,38 @@ def add_worksheet_data(data, worksheet):
     print(Fore.MAGENTA+'Data was updated successfuly!')
 
 
+def worksheet_sum(worksheet):
+    """
+    Function to sum the values inputted in the worksheets.
+    """
+    worksheet_sum_revenue = SHEET.worksheet(worksheet)
+    months = worksheet_sum_revenue.row_values(1)
+
+    sum_values = []
+    for month_index in range(1, len(months) + 1):
+        column_data = worksheet_sum_revenue.col_values(month_index)[1:]  # Exclude the header row
+        numeric_values = [float(value) if value != '' else 0 for value in column_data]
+        column_sum = sum(numeric_values)
+        sum_values.append(column_sum)
+
+    return sum_values
+
+
+def add_profit_data(data, worksheet):
+    """
+    Function to add data to the profit worksheet.
+    """
+    print(f'{Fore.CYAN}The {worksheet} is being updated!')
+    profit_worksheet = SHEET.worksheet(worksheet)
+    months = profit_worksheet.col_values(1)
+    month_index = months.index(data[0]) + 1
+    col_data = profit_worksheet.row_values(month_index)
+    last_col = len(col_data) + 1
+    profit_worksheet.update_cell(month_index, last_col, data[1])
+
+    print(Fore.MAGENTA+'Data was updated successfuly!')    
+
+
 def main():
     """
     Run all program functions.
@@ -134,7 +166,29 @@ def main():
     add_worksheet_data((month_input_revenue, revenue_input), 'revenue')
     add_worksheet_data((month_input_expense, expense_input), 'expenses')
 
-print(Back.BLACK + Fore.MAGENTA +'\033[1m'+"Welcome to Farmer Market Automation!\
-The program will help you to deal with expenses and revenue data, and so,\
-calculating the profit of your farmer market.'\033[0m'\n")
+    worksheet_sum_revenue = SHEET.worksheet('revenue')
+    months = worksheet_sum_revenue.row_values(1)
+    revenue_sums = worksheet_sum('revenue')
+    print("Sum values for each column in the 'revenue' worksheet:")
+    for month, sum_value in zip(months, revenue_sums):
+        sum_revenue_formatted = round(sum_value, 2)
+        print(f"{month}: {sum_revenue_formatted}")
+
+    worksheet_data_expense = SHEET.worksheet('expenses')
+    expense_months = worksheet_data_expense.row_values(1)  
+    expense_sums = worksheet_sum('expenses')
+    print("Sum values for each column in the 'expenses' worksheet:")
+    for month, sum_value in zip(expense_months, expense_sums):
+        sum_expense_formatted = round(sum_value, 2)
+        print(f"{month}: {sum_expense_formatted}") 
+
+    add_profit_data(sum_revenue_formatted, 'profit')    
+    add_profit_data(sum_expense_formatted, 'profit')
+
+
+print(Back.BLACK + Fore.MAGENTA + '\033[1m'+"Welcome to Farmer Market \
+Automation!'\033[0m'\n")
+print(Back.BLACK + Fore.MAGENTA + '\033[1m'+"The program will help you to \
+deal with expenses and revenue data, and so, calculating the profit of your \
+farmer market.'\033[0m'\n")
 main()
